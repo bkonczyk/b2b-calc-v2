3a9dd22d5d456fb4f078719cf28b2d38fbee6816
Bartosz Kończyk <bartkon@gmail.com>
2026-01-29 10:49:09 +0100

    unify spacing

--- src/app/components/layout/footer/footer.component.ts
+++ src/app/components/layout/footer/footer.component.ts
@@ -1,7 +1,4 @@
-import {Component, inject} from '@angular/core';
-import {CommonModule} from '@angular/common'; // Potrzebne dla ngClass itp.
-import {B2bCalculatorService} from '../../../services/b2b-calculator.service';
-import {TAX_FORM_OPTIONS, ZUS_OPTIONS} from '../../../models/b2b-types';
+import {Component} from '@angular/core';
 
 
 @Component({
